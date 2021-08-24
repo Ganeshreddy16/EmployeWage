b159153812b0cd83cf5e6fd259ade1f30e9733a8
Ganesh Reddy <reddyganesh125@gmail.com>
2021-08-24 09:41:09 +0530

    Added: Project Structure

---
+++ src/main/java/org/blz/wage/App.java
@@ -0,0 +1,13 @@
+package org.blz.wage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/blz/wage/EmployeWageBuilder.java
@@ -0,0 +1,9 @@
+package org.blz.wage;
+
+public class EmployeWageBuilder {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wages");
+
+    }
+}
